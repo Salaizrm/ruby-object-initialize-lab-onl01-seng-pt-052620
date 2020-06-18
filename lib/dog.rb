ad3f4dc00bef2d232428dddb6584b55770527815
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-18 03:42:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
 
-  def initialize(name, breed)
+  def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
@@ -9,7 +9,7 @@ class Dog
     @name
   end
 
-  def breed=(mutt)
+  def breed=(breed)
     @breed
   end
 
